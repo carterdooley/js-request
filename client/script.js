b24e43882e82fbfcb32ad0cbe2f80c6e0575a1f9
carter dooley <carter.j.dooley43@gmail.com>
2021-10-28 16:59:51 -0500

    Edit to 11

--- client/script.js
+++ client/script.js
@@ -2,12 +2,14 @@
 //THE TEST SERVER IS RUNNING ON LOCALHOST:3000//
 ////////////////////////////////////////////////
 
+//REMEMBER TO START SERVER IN CONSOLE. CD TO DIRECTORY THE RUN 'NODE SERVER/INDEX.JS'
+
+
 // const { default: axios } = require("axios")
 
 // PROBLEM 1
 /*
     In the index.html file in this folder there is a button with an id of 'say-hello-button'!
-
     Use querySelector to select that button and save it to a variable called sayHelloButton
 */
 
@@ -40,9 +42,7 @@ sayHelloButton.addEventListener("mouseover", changeBtnClr)
 // PROBLEM 3
 /*
     Now you can see that the button colors change, but they do not change back when we take the mouse off of the button.
-
     Write another function that changes the button back to its original colors. #EFEFEF for the background and black for the text.
-
     Attach another listener that fires your second function when the mouseout event occurs on the button
 */
 
@@ -58,7 +58,6 @@ sayHelloButton.addEventListener("mouseout", changeBtnBack)
 // PROBLEM 4
 /*
     Now lets see if we can make a request to our server when we click the button
-
     Add a 3rd event listener to sayHelloButton and trigger the sayHello function when the button is clicked
 */
 
@@ -107,9 +106,7 @@ document.getElementById('animals-button').addEventListener('click', ohMy)
 // PROBLEM 6 
 /*
     Now lets see if you can send a request param! inside repeatMyParam function below  make get request to 'http://localhost:3000/repeat/{SOMEPARAM}', but with a string instead of {SOMEPARAM}.  
-
     The function that runs when this request is made will return whatever parameter you sent 
-
     Handle the promise returned from the request with a .then, which will take in a callback -- the callback function should print the response.data.
     
     Outside of the function, select the button with the id "repeat-button" and add a click event listener that calls the repeatMyParam function.
@@ -144,9 +141,7 @@ document.getElementById('repeat-button').addEventListener('click', repeatMyParam
 // PROBLEM 8
 /*
     Time to attach a query to our request!
-
     Write a function that makes a get request to 'http://localhost:3000/query-test', with a query of your choice on the end!
-
     Outside of your new function, select the button with the id "query-button" and add a click event listener that calls your function.
 */
 
@@ -171,7 +166,6 @@ document.getElementById('query-button').addEventListener('click', queryFunct)
 // PROBLEM 9
 /* 
     Back in the ohMy function on Problem 5, replace the console log in the promise's callback with a for loop that loops over res.data. 
-
     On each iteration of the loop, create a new p element. Set its textContent equal the string at the current index (i) and then append the new p element onto the document's body. 
 */
 
@@ -181,9 +175,7 @@ document.getElementById('query-button').addEventListener('click', queryFunct)
 // PROBLEM 10 
 /*
     In the function that you wrote for Problem 8, change the URL to test a couple different scenarios. 
-
     1: Send no queries on the URL -- what happened? 
-
     2: Send more than 1 query on the URL -- what happened? 
 */
 
@@ -199,30 +191,26 @@ document.getElementById('query-button').addEventListener('click', queryFunct)
 //PROBLEM 11
 /*
     You are going to add the ability to POST to the server. You'll need to create a small form and write a function that makes a post request. Then you'll attach that function to the submit event on the form. We'll be creating a list of foods. 
-
     In the index.html file inside of the client folder, create a form with one text input field and a button. The input field should have a placeholder that tells the user to enter a food. And the button should indicate that it will add food into a list. 
-
     In this file (script.js), create a function called createFood. 
     
     Inside the function, select the input you just created in the HTML and save it to a variable called foodInput. 
     
     Next, create an object called body inside the function. It should have one key-value pair. The key should be newFood (make sure to match the case and spelling exactly) and the value should be the value of the food input. 
-
     Now make an axios post request to /food. Inside the parentheses where you passed the URL in, pass in body as the second argument. 
-
     Use a .then to handle the promise returned from the axios call. Pass a callback function to the .then. Inside that callback, console log the res.data. 
-
     Based on what we did earlier to display this type of data, write code that will display the response in your HTML document. 
 */
 
 // CODE HERE 
 
-const createFood = () => {
+const createFood = (event) => {
+    event.preventDefault()
     let foodInput = document.querySelector('input')
     let body = {
         'newFood': foodInput
     }
-    axios.get('http://localhost:3000/food', body)
+    axios.post('http://localhost:3000/food', body)
     .then(res => {
         console.log('RESPONSE', res.data)
         // let p = document.createElement('p')
